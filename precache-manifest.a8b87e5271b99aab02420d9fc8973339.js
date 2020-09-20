032cad045d9b27ffa3baef38e9b823cd64422189
Ivan Alenin <aiscorp@yandex.ru>
2020-09-21 01:08:55 +0300

    Updates

---
+++ precache-manifest.a8b87e5271b99aab02420d9fc8973339.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71497a211e4405c63d659b1f4f81a096",
+    "url": "/fugr-ru-react-test/index.html"
+  },
+  {
+    "revision": "6d71f22ae0bea22ec522",
+    "url": "/fugr-ru-react-test/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "39e66de21a2741afe808",
+    "url": "/fugr-ru-react-test/static/js/2.41a19348.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fugr-ru-react-test/static/js/2.41a19348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d71f22ae0bea22ec522",
+    "url": "/fugr-ru-react-test/static/js/main.0e6cc406.chunk.js"
+  },
+  {
+    "revision": "6c49e45d7acef5724baa",
+    "url": "/fugr-ru-react-test/static/js/runtime-main.66173708.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/fugr-ru-react-test/static/media/logo.ee7cd8ed.svg"
+  }
+]);
