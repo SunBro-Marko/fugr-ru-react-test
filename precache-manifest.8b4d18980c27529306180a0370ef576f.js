83eec100ff85ed5762e1d5a4ccaf8849418d1520
aiscorp <43959850+aiscorp@users.noreply.github.com>
2020-09-21 14:13:11 +0000

    Deploying to gh-pages from  @ 85498af5800d7ca9b22bfdf3598d8ce3a8ff3466 🚀

---
+++ precache-manifest.8b4d18980c27529306180a0370ef576f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b30287e05c0b0b4488ece48971e5db",
+    "url": "/fugr-ru-react-test/index.html"
+  },
+  {
+    "revision": "f432119504482e24ae8a",
+    "url": "/fugr-ru-react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "39e66de21a2741afe808",
+    "url": "/fugr-ru-react-test/static/js/2.41a19348.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fugr-ru-react-test/static/js/2.41a19348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f432119504482e24ae8a",
+    "url": "/fugr-ru-react-test/static/js/main.ea978155.chunk.js"
+  },
+  {
+    "revision": "6c49e45d7acef5724baa",
+    "url": "/fugr-ru-react-test/static/js/runtime-main.66173708.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fugr-ru-react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.816e7217cbefde3bebec092e00df70a3.js"
+  "/fugr-ru-react-test/precache-manifest.8b4d18980c27529306180a0370ef576f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fugr-ru-react-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
