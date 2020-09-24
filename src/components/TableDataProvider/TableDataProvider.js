bee116f0811ec77971562d3ae9e88b5cf62ec9c3
Ivan Alenin <aiscorp@yandex.ru>
2020-09-24 21:53:52 +0300

    Patch to TableDataProvider

--- src/components/TableDataProvider/TableDataProvider.js
+++ src/components/TableDataProvider/TableDataProvider.js
@@ -3,8 +3,8 @@ import TablePage from '../../pages/TablePage'
 
 export class TableDataProvider extends React.Component {
   fetchUrl = {
-    small: `http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`,
-    large: `http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
+    large: `http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`,
+    small: `http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
   }
 
   constructor(props) {
@@ -18,7 +18,7 @@ export class TableDataProvider extends React.Component {
   }
 
   componentDidMount() {
-    fetch(this.state.fetchUrl)
+    fetch(this.state.fetchUrl, {mode: 'no-cors'})
       .then(res => res.json())
       .then(
         (result) => {
